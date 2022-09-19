30d5a0bf68597a815067e31dbcf9e916e4c1f569
der-kaffe <jcardenas2022@alu.uct.cl>
2022-09-19 19:36:55 -0300

    Fuente de la letra e icono en el header

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,36 +1,36 @@
-'use strict'; 
-const e = React.createElement; 
+'use strict';
+const e = React.createElement;
 
 class Timer extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = { seconds: 0 };
-    }
-  
-    tick() {
-      this.setState(state => ({
-        seconds: state.seconds + 1
-      }));
-    }
-  
-    componentDidMount() {
-      this.interval = setInterval(() => this.tick(), 1000);
-    }
-  
-    componentWillUnmount() {
-      clearInterval(this.interval);
-    }
-  
-    render() {
-      return e(
-        "div",
-        null,
-        "Pagina activa durante: ",
-        this.state.seconds,
-        " Segundos"
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.state = { seconds: 0 };
   }
-  
-const domContainer = document.querySelector('#timer');  
+
+  tick() {
+    this.setState(state => ({
+      seconds: state.seconds + 1
+    }));
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return e(
+      "div",
+      null,
+      "Pagina activa durante: ",
+      this.state.seconds,
+      " Segundos"
+    );
+  }
+}
+
+const domContainer = document.querySelector('#timer');
 ReactDOM.render(e(Timer), domContainer);
