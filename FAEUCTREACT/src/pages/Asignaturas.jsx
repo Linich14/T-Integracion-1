aee5425c2d4d6d586d59a0b17725747b0ed9504d
Jose <jjimenez2020@alu.uct.cl>
2022-10-10 23:32:21 -0300

    "YES"

---
+++ FAEUCTREACT/src/pages/Asignaturas.jsx
@@ -0,0 +1,14 @@
+
+  import React from 'react'
+  import './Asignaturas.css';
+
+
+  
+  const Asignaturas = () => {
+    return (
+      <div>asignaturas</div>
+    )
+  }
+  
+  export default Asignaturas
+  
