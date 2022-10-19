793cab4b0de11191a7b34d7cc81fa7e3f52e6390
der-kaffe <jcardenas2022@alu.uct.cl>
2022-10-18 22:41:18 -0300

    cuenta

--- FAEUCTREACT/src/components/firebase.js
+++ FAEUCTREACT/src/components/firebase.js
@@ -1,11 +1,18 @@
 
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import {getFirestore} from 'firebase/firestore'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage, ref } from "firebase/storage";
 
 
 const firebaseConfig = {
-
+  apiKey: "AIzaSyAQQ6QbZCYfH5ZX9DB3sruh_f-yhHq69P8",
+  authDomain: "faeuct-test.firebaseapp.com",
+  databaseURL: "https://faeuct-test-default-rtdb.firebaseio.com/",
+  projectId: "faeuct-test",
+  storageBucket: "faeuct-test.appspot.com",
+  messagingSenderId: "569802921949",
+  appId: "1:569802921949:web:c0510e7b29453b9ff28834"
   //credenciales firebase
 };
 
@@ -13,3 +20,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app)
+const storage = getStorage(app);
