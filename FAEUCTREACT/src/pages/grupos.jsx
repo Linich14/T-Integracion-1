095ada4269815496449346fa5c586c8919ed5e1d
der-kaffe <jcardenas2022@alu.uct.cl>
2022-11-08 15:53:29 -0300

    Update grupos.jsx

--- FAEUCTREACT/src/pages/grupos.jsx
+++ FAEUCTREACT/src/pages/grupos.jsx
@@ -1,57 +1,70 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Menu from '../components/Menu'
 import './grupos.css'
-import { Link } from 'react-router-dom';
 import { UserAuth } from '../context/AuthContext';
 import Photo from './fotosperfil'
+import PostFrom from '../components/form';
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { db } from '../components/firebase'
+
+const Grupos = () => {
+    const { user } = UserAuth();
+    const [blogs, setblogs] = useState([]);
+
+    useEffect(() => {
+        const blogref = collection(db, "Blogs");
+        const q = query(blogref, orderBy("createdat", "desc"))
+        onSnapshot(q, (snapshot) => {
+            const allBlogs = snapshot.docs.map((docs) => (
+                {
+                    id: docs.id,
+                    ...docs.data()
+                }
+            ))
+            setblogs(allBlogs)
+        })
+    }, [])
+    return (
+        <div>
+            <br /><br /><br /><br />
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-2'>
+                        <br /><Menu /></div>
+                    <div className='col-10'>
+                        <div className='row rounded' style={{ backgroundColor: " rgba(239, 247, 168, 0.89)" }}>
+                            <div className=''>
+                                <PostFrom />
+                                <hr />
+                            </div>
+                        </div>
+                        <br />
+                        <div className='row rounded' style={{ backgroundColor: " rgba(239, 247, 168, 0.89)" }}>
+                            <h1>aqui van los posteos</h1>
+                            <div className='py-1' style={{ backgroundColor: " rgba(239, 247, 168, 0.89)" }}>
+                                {blogs.length === 0 ? (<p>No hay blogs</p>) : (blogs.map((blog) =>
+                                    <div className='blog-cont' key={blog.id}>
+                                        <hr />
+                                        <div className='section1'>
+                                            <h2>{blog.title}</h2>
+                                            <p>Publicado por: {user?.displayName}</p>
+                                        </div>
+                                        <div className='section2'><p>{blog.description}</p></div>
+                                        <div className='section3'><img src={blog.imgurl} width='200' height='200' alt=' . . .' /></div>
+                                        <div className='section4'>
+                                            <p>- {blog.createdat}</p>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
 
-const grupos = () => {
-  const { user } = UserAuth();
-  return (
-<div>
-<br /><br /><br /><br />
-<div className='container'> 
-
-<div className='row'>
-    <div className='col-2'>
-        <br /><Menu/></div>
-    <div className='col-10'>
-        <div className='row rounded'style={{backgroundColor:" rgba(239, 247, 168, 0.89)"}}>
-            <div className=''>
-            <h2>Ingrese su nuevo posteo</h2>
-                <form action="">
-                    <textarea name="" id="" cols="100" rows="5"></textarea>
-                    
-                </form>
-                <button className='btn btn-primary'  >Publicar</button>
-                <hr />
             </div>
         </div>
-        <br />
-        <div className='row rounded' style={{backgroundColor:" rgba(239, 247, 168, 0.89)"}}>
-            <h1>aqui van los posteos</h1>
-            <div className='py-1' style={{backgroundColor:" rgba(239, 247, 168, 0.89)"}}>
-             <Photo url={user?.photoURL} className="avatar" /><pre><h2>mucho texto del post</h2></pre>
-             <Photo url="https://imgur.com/oxiFX0j" />
-            </div>
-            <hr />
-            <div className='py-1' style={{backgroundColor:" rgba(239, 247, 168, 0.89)"}}>
-               
-             <Photo url={user?.photoURL} className="avatar"/><pre><h2>mucho texto del post</h2></pre>
-             <Link to="https://imgur.com">
-                <Photo url="https://i.imgur.com/oxiFX0j.jpg" />
-             </Link>
-             
-            </div>
-        </div>
-    </div>
-</div>
-
-      
-</div>
-</div>
-  )
+    )
 }
 
-export default grupos
+export default Grupos;
